833edc21a5e287ae5dbdd85907d923347c710641
danilo.adventista <danilo.adventista@gmail.com>
2022-10-10 18:40:23 -0300

    Sign Up, Log In and Log Out

--- app/src/main/java/com/back4app/java/example/MainActivity.java
+++ app/src/main/java/com/back4app/java/example/MainActivity.java
@@ -7,12 +7,15 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.parse.ParseAnalytics;
+import com.parse.SignUpCallback;
 
 import java.util.List;
 
@@ -23,42 +26,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* inserted from the course */
-        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+        ParseUser.logOut();
 
-        TextView textView = findViewById(R.id.textView);
-        ParseObject firstObject = new  ParseObject("FirstClass");
-        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+        if (ParseUser.getCurrentUser() != null){
+            Log.d("currentUser", "User Logged In " + ParseUser.getCurrentUser().getUsername());
+        }else{
+            Log.d("currentUser", "User Not Logged In ");
+        }
 
-        firstObject.saveInBackground(e -> {
-            if (e != null){
-                Log.e("MainActivity", e.getLocalizedMessage());
-            }else{
-                Log.d("MainActivity","Object saved.");
-                textView.setText(String.format("Object saved. %s", firstObject.getObjectId()));
-            }
-        });
 
-        /* query with object id  2CjaznUgWh*/
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("FirstClass");
-        query.whereEqualTo("message", "Test");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> firstClassList, ParseException e) {
-                if (e == null) {
-                    Log.d("score", "Retrieved " + firstClassList.size() + " FirstClass");
-                    for (ParseObject o: firstClassList
-                         ) {
-                        int myValue = o.getInt("UpdatedValue");
-                        o.put("UpdatedValue",myValue+1);
-                        o.saveInBackground();
-                        Log.d("UpdatedValue", "myValue: " + myValue+1);
-                    }
-                } else {
-                    Log.d("score", "Error: " + e.getMessage());
+        /*
+        ParseUser.logInInBackground("Danilo", "asdasd", new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null){
+                    Log.d("Login", "Successfull");
+                }else{
+                    Log.d("Login", "Failed " + e.toString());
                 }
+
             }
         });
+        */
 
+        /*
+        ParseUser user = new ParseUser();
+        user.setUsername("Danilo");
+        user.setPassword("myPass");
+        user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Log.d("Sign Up", "Successfull");
+                    } else {
+                        Log.d("score", "Error: " + e.getMessage());
+                    }
+                }
+            });
+        ;
+
+        ;
+        */
 
 
     }
