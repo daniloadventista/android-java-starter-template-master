9288c7e457d1e667663217be84b8e4ec68f88cd2
danilo.adventista <danilo.adventista@gmail.com>
2022-10-10 18:05:52 -0300

    FIRST QUERIES

--- app/src/main/java/com/back4app/java/example/MainActivity.java
+++ app/src/main/java/com/back4app/java/example/MainActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.parse.ParseAnalytics;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
             }else{
                 Log.d("MainActivity","Object saved.");
                 textView.setText(String.format("Object saved. %s", firstObject.getObjectId()));
+            }
+        });
+
+        /* query with object id  2CjaznUgWh*/
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("FirstClass");
+        query.whereEqualTo("message", "Test");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> firstClassList, ParseException e) {
+                if (e == null) {
+                    Log.d("score", "Retrieved " + firstClassList.size() + " FirstClass");
+                    for (ParseObject o: firstClassList
+                         ) {
+                        int myValue = o.getInt("UpdatedValue");
+                        o.put("UpdatedValue",myValue+1);
+                        o.saveInBackground();
+                        Log.d("UpdatedValue", "myValue: " + myValue+1);
+                    }
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
             }
         });
 
